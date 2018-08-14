7e7576368d2dde97200edb51c45e3e689e06cfb8
DwaynesWorld <kthompson713@gmail.com>
2018-08-13 23:32:33 -0500

    Added env var, error sent to error stream

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
 
     if let Err(e) = grepper::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         std::process::exit(1);
     }
 }
